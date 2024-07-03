73bd91989c3368cb2fce58029b6156492f38d8f8
UX403-B03 UX403-B03 <p003656>
2024-07-03 12:35:05 +0000

    new

---
+++ objectpage.student03/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("objectpage.student03.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
